ea8ba12d0754c186ab17ebda47b00dfe341017a7
YuKun <78700194@qq.com>
2018-09-22 15:06:22 +0800

    添加植物显示cell

---
+++ PlantBook/PlantBook/ViewControllers/PlantListViewControllers/TableViewCells/PlantTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PlantTableViewCell.swift
+//  PlantBook
+//
+//  Created by yukun on 2018/9/22.
+//  Copyright © 2018 SouthWest University-yukun. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var plantImageView: UIImageView!
+    
+    @IBOutlet weak var plantNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
